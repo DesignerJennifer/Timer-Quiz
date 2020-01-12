9457d996c0db6732c282a9881d2a1871e25ec18b
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-01-11 23:50:03 -0600

    quiz Questions

---
+++ questions.js
@@ -0,0 +1,19 @@
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Question",
+      choices: ["answer1", "answer2", "answer3", "answer4"],
+      answer: "answer3"
+    }
+  ];
+  
